3e40901bb46287d730d24c438bc925452ed002ef
Reyan Asghar <iamreyanasghar@gmail.com>
2026-08-01 09:36:53 +0500

    robot.txt url changed

--- buypakistani/urls.py
+++ buypakistani/urls.py
@@ -2,8 +2,10 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic.base import TemplateView
+from django.views.static import serve as static_serve
+import os
 
 urlpatterns = [
-    path('robots.txt', TemplateView.as_view(template_name='robots.txt', content_type='text/plain')),
+    path('robots.txt', static_serve, {'document_root': settings.BASE_DIR / 'static', 'path': 'robots.txt'}),
     path('', include('core.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
